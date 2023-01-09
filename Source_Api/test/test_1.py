a2a7c9de512ff831a12245bca4a35b0a9a6532ba
michael4980 <migel1999@bk.ru>
2023-01-09 17:19:14 +0300

    add config

--- Source_Api/test/test_1.py
+++ Source_Api/test/test_1.py
@@ -1,11 +1,14 @@
 from chahhel_source import Handler, Source
+from config import load_config
+
+config = load_config('source.ini')
 
 def test_right_method_get():
     assert Handler.do_GET == Source.current_state, 'didn`t get state info'
     
 def test_rught_method_post():
-    path_1 = r'http://127.0.0.1:8080/switching_on/'
-    path_2 = r'http://127.0.0.1:8080/turn_off/'
+    path_1 = f'{config.api_src.host}/switching_on/'
+    path_2 = f'{config.api_src.host}/turn_off/'
     if path_1:
         assert Handler.do_POST == Source.switching_on, 'wrong switching method '
     elif path_2:
