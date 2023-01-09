7662733f2e01707343f2182e630d04f6c6186643
michael4980 <migel1999@bk.ru>
2023-01-09 12:00:04 +0300

    final

--- Source_Api/test/test_1.py
+++ Source_Api/test/test_1.py
@@ -1,4 +1,12 @@
+from chahhel_source import Handler, Source
 
-
-def test_equal():
-    assert 1==1,"good job"
+def test_right_method_get():
+    assert Handler.do_GET == Source.current_state, 'didn`t get state info'
+    
+def test_rught_method_post():
+    path_1 = r'http://127.0.0.1:8080/switching_on/'
+    path_2 = r'http://127.0.0.1:8080/turn_off/'
+    if path_1:
+        assert Handler.do_POST == Source.switching_on, 'wrong switching method '
+    elif path_2:
+        assert Handler.do_POST == Source.switching_on, 'wrong turning off method' 
